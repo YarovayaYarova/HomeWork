b271898598b0d6d933ecc0d13273118f96796412
Anna Yarova <yarovaya.yarova@gmail.com>
2022-12-18 22:36:10 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,36 @@
+public class Hello {
+    public static void main(String[] args) {
+        //Write a Java program to print 'Hello' on the screen and then print your name on a separate line
+        String name = "Anna";
+
+        //Write a Java program to print the result of the following operations.
+        int a = -5 + 8 * 6;
+        int b1 = -3 * 5;
+        double b2 = (double) b1 / 8;
+        double b = 20 + b2;
+
+        // Write a java program with 5 decimal variables. Multiply all 5 of them and print results
+        double numA = 1.2;
+        double numB = 2.3;
+        double numC = 3.4;
+        double numD = 4.5;
+        double numE = 5.6;
+        double result = numA * numB * numC * numD * numE;
+
+
+        System.out.println("Hello");
+        System.out.println(name);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println(numA + " * " + numB + " * " + numC + " * " + numD + " * " + numE + " = " + result);
+        }
+
+    }
+
+
+
+
+ 
+
+
+
